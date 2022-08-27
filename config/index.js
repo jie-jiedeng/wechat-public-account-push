cbbd2630d5e7f20f8b28a126da8d277924a1346a
jie-jiedeng <77815362+jie-jiedeng@users.noreply.github.com>
2022-08-27 13:33:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,26 +1,26 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wxeb7fc20d8fbd2f99",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "d8d358a5bc88b1304bf26ae74caa3a12",
     // 模板消息id
-    templateId: "",
+    templateId: "	Jj4t_HJT4oxGOiPh01DvDGhs0oPHNdC93NOqr5z2H4E",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [ogy2p5t2W4JSVUlwGYusmehHrWi4],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "浙江",
     // 所在城市
-    city: "惠州",
+    city: "台州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
+      {"name": "老婆", "year": "2001", "date": "05-03", "type": 'new'},
       {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
+    loveDate: "2022-02-10",
     // 结婚纪念日
     marryDate: "2020-01-04"
     }
